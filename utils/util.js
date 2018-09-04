ef325ed9c93bc0bb6a4af32833ea281330498d7a
leiqiang123 <1229949407@qq.com>
2018-09-04 09:00:14 +0800

    昨天做得云书代码

---
+++ utils/util.js
@@ -0,0 +1,29 @@
+const baseUrl = "http://m.yaojunrong.com"
+
+
+
+const fetch = {
+  http(url,method,data){
+    return new Promise((resolve,reject)=>{
+      wx.request({
+        url: baseUrl + url,
+        method,
+        data,
+        header: {
+          "content-type": "application/json"
+        },
+        success(res) {
+          resolve(res.data)
+        },
+        fail(err) {
+          reject(err)
+        }
+      })
+    })    
+  },
+  get(url,data){
+   return this.http(url,"GET",data)
+  }
+}
+
+exports.fetch = fetch;
